d652fa65e39e52f487e1aed2792f4684fbfa104e
Bahar Arman <Emma.symlone@gmail.com>
2026-06-12 23:21:47 +1000

    Built a frontend working skeleton
    - added neccessary packages
    - fetch data from backend (set up the end to end connection)

---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,12 @@
+type Job = {
+  id: string
+  status: number
+  dateApplied: string
+  companyName: string
+  positionTitle: string
+  jobUrl: string
+  location: string
+  source: string
+  createdAt: string
+  updatedAt: string
+}
